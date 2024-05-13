84050ae91ff812aee20c880ea5ceb53a08da2623
Debanshh <babydeka31@gmail.com>
2024-05-13 19:57:34 +0530

    Create Hello.php

---
+++ Hello.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title> Welcome To Server Programming</title>
+  </head>
+  <body>
+    //the command echo display the content given inside the double quotes 
+    <?php echo "Hello PHP!;">?
+  </body>
+</html>
